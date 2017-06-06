08619071db73c90aa50c9df407b7166b67787053
lopezfjose <jflopezfernandez@gmail.com>
2017-06-06 14:54:02 -0400

    About to start data structures

--- main.c
+++ main.c
@@ -5,23 +5,27 @@
 #include <inttypes.h>
 
 
+struct _a {
+	int a; 				// 4 bytes
+	int c; 				// 4 bytes
+	double b; 			// 8 bytes
+	long long int d; 	// 8 bytes
+};
+
+struct _b {
+	int a; 				// 4 bytes
+	double b; 			// 8 bytes
+	int c; 				// 4 bytes
+	long long int d; 	// 8 bytes
+};
+
+#define COUNT 4
+
+
 int main()
 {
 	system("cls");
 
-	char *s;
-	int i = sizeof("hello");
-	s = (char *) malloc(i);
 
-	strncpy(s, "hello", i);
-	printf("s is %s\n", s);
-
-	realloc(s, 12);
-	strncpy(s, "hello", i);
-	strcat(s, " world");
-
-	printf("s is now %s\n", s);
-
-	free(s);
     return EXIT_SUCCESS;
 }
